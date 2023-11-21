63123644fe8ea2ab807f8063a39396735dbe88ea
ManuelBasso <manuel.basso.89@gmail.com>
2023-11-21 13:19:50 +0100

    added a method for every operation

--- _25Testing/src/Main.java
+++ _25Testing/src/Main.java
@@ -9,20 +9,44 @@ public class Main {
         OffsetDateTime date = OffsetDateTime.parse("2023-03-01T13:00:00Z");
         System.out.println("Initial date: " + date);
 
-        //aggiungi un anno, sottrai un mese, aggiungi 7 giorni
-        OffsetDateTime newDate = changeDate(date);
-        System.out.println("New date: " + newDate);
+        //aggiungi un anno
+        OffsetDateTime newDate = addYear(date);
+        System.out.println("Add 1 year: " + newDate);
+
+        //sottrai un mese
+        newDate = subtractMonth(newDate);
+        System.out.println("Minus 1 month: " + newDate);
+
+        //aggiungi 7 giorni
+        newDate = add7days(newDate);
+        System.out.println("Plus 7 days:" + newDate);
 
         //Stampa il risultato localizzata per l'Italia
         String dateString = newDate.format(DateTimeFormatter.ofPattern("EEEE dd MMMM yyyy", Locale.ITALY));
         System.out.println("Date in Italian: "+ dateString);
     }
 
-    public static OffsetDateTime changeDate(OffsetDateTime date) {
+     static OffsetDateTime add7days(OffsetDateTime newDate) {
+        if (newDate == null) {
+            return null;
+        } else {
+            return newDate.plusDays(7);
+        }
+    }
+
+     static OffsetDateTime subtractMonth(OffsetDateTime date) {
         if (date == null) {
             return null;
         } else {
-            return date.plusYears(1).minusMonths(1).plusDays(7);
+            return date.minusMonths(1);
+        }
+    }
+
+    public static OffsetDateTime addYear(OffsetDateTime date) {
+        if (date == null) {
+            return null;
+        } else {
+            return date.plusYears(1);
     }
 
 
--- _25Testing/src/MainTest.java
+++ _25Testing/src/MainTest.java
@@ -10,20 +10,37 @@ class MainTest {
 
 
     @Test
-    void changeDate_with_month_equal_1() {
-        OffsetDateTime result = testing.changeDate(OffsetDateTime.parse("2010-01-05T13:00:00Z"));
-        assertEquals(OffsetDateTime.parse("2010-12-12T13:00:00Z"), result);
-    }
-
-    @Test
-    void changeDate_with_february28() {
-        OffsetDateTime result = testing.changeDate(OffsetDateTime.parse("2010-02-28T13:00:00Z"));
-        assertEquals(OffsetDateTime.parse("2011-02-04T13:00:00Z"), result);
+    void addYear_from_2010() {
+        OffsetDateTime result = testing.addYear(OffsetDateTime.parse("2010-01-05T13:00:00Z"));
+        assertEquals(OffsetDateTime.parse("2011-01-05T13:00:00Z"), result);
     }
 
     @Test
     void changeDate_null() {
-        OffsetDateTime result = testing.changeDate(null);
+        OffsetDateTime result = testing.addYear(null);
+        assertNull(result);
+    }
+    @Test
+    void subtractMonth() {
+        OffsetDateTime result = testing.subtractMonth(OffsetDateTime.parse("2010-01-05T13:00:00Z"));
+        assertEquals(OffsetDateTime.parse("2009-12-05T13:00:00Z"), result);
+    }
+
+    @Test
+    void subtractMonth_null() {
+        OffsetDateTime result = testing.subtractMonth(null);
+        assertNull(result);
+    }
+
+    @Test
+    void add7days_from_05012010() {
+        OffsetDateTime result = testing.add7days(OffsetDateTime.parse("2010-01-05T13:00:00Z"));
+        assertEquals(OffsetDateTime.parse("2010-01-12T13:00:00Z"), result);
+    }
+
+    @Test
+    void add7days_null() {
+        OffsetDateTime result = testing.add7days(null);
         assertNull(result);
     }
 
